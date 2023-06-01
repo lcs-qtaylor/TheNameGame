a1ab8dbf9ccf6478d02030d4f2a92dc69bdb416e
Quinlan Taylor <qtaylor@lcs.on.ca>
2023-06-01 14:30:09 -0400

    Initial Commit

---
+++ TheNameGame/TheNameGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheNameGameApp.swift
+//  TheNameGame
+//
+//  Created by Quin Taylor on 2023-06-01.
+//
+
+import SwiftUI
+
+@main
+struct TheNameGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
